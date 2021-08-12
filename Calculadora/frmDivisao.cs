62ad2e9855d029610a41f1ca60375c7f9442007a
Felippo <felippo2610@gmail.com>
2021-08-12 08:27:53 -0300

    Finalização exercicio

--- Calculadora/frmDivisao.cs
+++ Calculadora/frmDivisao.cs
@@ -19,6 +19,7 @@ namespace Calculadora
         private void btnResultado_Click(object sender, EventArgs e)
         {
             float result = float.Parse(txtNumero1.Text) / float.Parse(txtNumero2.Text);
+            lblResultado.Text = result.ToString();
         }
     }
 }
