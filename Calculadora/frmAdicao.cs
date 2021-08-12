f33c06ee44baefe6a60ee742b894d0522873bfe4
Kaua Lourenzon <lourenzonkaua@gmail.com>
2021-08-12 08:33:14 -0300

    adição

--- Calculadora/frmAdicao.cs
+++ Calculadora/frmAdicao.cs
@@ -18,6 +18,10 @@ namespace Calculadora
 
         private void btnResultado_Click(object sender, EventArgs e)
         {
+            int N1 = int.Parse(txtNumero1.Text);
+            int N2 = int.Parse(txtNumero2.Text);
+
+            int result = N1 + N2; 
 
         }
     }
