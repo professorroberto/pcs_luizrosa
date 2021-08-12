1c3f69f2cec37c55c2b8deec4d40cf840d75d867
Kaua Lourenzon <lourenzonkaua@gmail.com>
2021-08-12 09:07:05 -0300

    Multiplicação

--- Calculadora/frmMultiplicacao.cs
+++ Calculadora/frmMultiplicacao.cs
@@ -15,5 +15,13 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            int N1 = int.Parse(txtNumero1.Text);
+            int N2 = int.Parse(txtNumero2.Text);
+
+            int resultmult = N1 * N2;
+        }
     }
 }
