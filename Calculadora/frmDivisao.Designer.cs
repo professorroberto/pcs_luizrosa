39727a5a5360f4d70d16e3d04f21e0a6a22c2b74
Kaua Lourenzon <lourenzonkaua@gmail.com>
2021-08-12 08:06:53 -0300

    desenho dos formulários

--- Calculadora/frmDivisao.Designer.cs
+++ Calculadora/frmDivisao.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label2
@@ -50,11 +54,49 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Número 1";
             // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(118, 23);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 11;
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(12, 23);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 10;
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResultado.Location = new System.Drawing.Point(12, 60);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(206, 39);
+            this.btnResultado.TabIndex = 9;
+            this.btnResultado.Text = "Resultado";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 112);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(206, 41);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "lblResultado";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // frmDivisao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "frmDivisao";
@@ -68,5 +110,9 @@
 
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
