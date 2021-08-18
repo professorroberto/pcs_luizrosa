837c5c13dbabe21ec5c32ffc5126ab4408ac0b9f
Professor Roberto Ferreira <professorroberto.luizrosa@gmail.com>
2021-08-18 19:26:10 -0300

    CORRIDA 1

--- Calculadora/frmPROFESSOR_ROBERTO.Designer.cs
+++ Calculadora/frmPROFESSOR_ROBERTO.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmPROFESSOR_ROBERTO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmPROFESSOR_ROBERTO";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frmPROFESSOR_ROBERTO";
+            this.Text = "123456789";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
