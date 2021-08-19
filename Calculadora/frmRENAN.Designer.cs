28f519f70db5064f09a2ae6731f483a9f65933fb
RenanLopesCruz <renanroyale123@gmail.com>
2021-08-19 08:12:28 -0300

    CORRIDA 3 RENAN

--- Calculadora/frmRENAN.Designer.cs
+++ Calculadora/frmRENAN.Designer.cs
@@ -31,13 +31,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtN3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.lblRES = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -67,26 +67,26 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Número 3";
             // 
-            // textBox1
+            // txtN1
             // 
-            this.textBox1.Location = new System.Drawing.Point(41, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(85, 22);
-            this.textBox1.TabIndex = 3;
+            this.txtN1.Location = new System.Drawing.Point(41, 59);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(85, 22);
+            this.txtN1.TabIndex = 3;
             // 
-            // textBox2
+            // txtN2
             // 
-            this.textBox2.Location = new System.Drawing.Point(145, 59);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(85, 22);
-            this.textBox2.TabIndex = 4;
+            this.txtN2.Location = new System.Drawing.Point(145, 59);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(85, 22);
+            this.txtN2.TabIndex = 4;
             // 
-            // textBox3
+            // txtN3
             // 
-            this.textBox3.Location = new System.Drawing.Point(253, 59);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(85, 22);
-            this.textBox3.TabIndex = 5;
+            this.txtN3.Location = new System.Drawing.Point(253, 59);
+            this.txtN3.Name = "txtN3";
+            this.txtN3.Size = new System.Drawing.Size(85, 22);
+            this.txtN3.TabIndex = 5;
             // 
             // button1
             // 
@@ -106,36 +106,37 @@
             this.button2.Text = "SUBTRAIR";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnMultiplicar
             // 
-            this.button3.Location = new System.Drawing.Point(255, 93);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 33);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "MULTIPLICAR";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Location = new System.Drawing.Point(255, 93);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(83, 33);
+            this.btnMultiplicar.TabIndex = 8;
+            this.btnMultiplicar.Text = "MULTIPLICAR";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label4
+            // lblRES
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(163, 163);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 17);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "resultado";
+            this.lblRES.AutoSize = true;
+            this.lblRES.Location = new System.Drawing.Point(163, 163);
+            this.lblRES.Name = "lblRES";
+            this.lblRES.Size = new System.Drawing.Size(67, 17);
+            this.lblRES.TabIndex = 9;
+            this.lblRES.Text = "resultado";
             // 
             // frmRENAN
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(548, 431);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblRES);
+            this.Controls.Add(this.btnMultiplicar);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtN3);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -153,12 +154,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtN3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label lblRES;
     }
 }
