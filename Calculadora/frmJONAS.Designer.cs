9a0fbb8547bfad3820d20d104b274380b786a514
unknown <jonascpj@gmail.com>
2021-08-19 08:07:57 -0300

    part 2

--- Calculadora/frmJONAS.Designer.cs
+++ Calculadora/frmJONAS.Designer.cs
@@ -28,19 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(428, 178);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(127, 44);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "subtrair";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(277, 180);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(122, 41);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "somar";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(588, 178);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(113, 43);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "multiplicar";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "numero 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "numero 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(597, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "numero 3";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(289, 83);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(109, 20);
+            this.txt1.TabIndex = 6;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(588, 83);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(109, 20);
+            this.txt3.TabIndex = 7;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(428, 83);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(109, 20);
+            this.txt2.TabIndex = 8;
             // 
             // frmJONAS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(775, 386);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
             this.Name = "frmJONAS";
-            this.Text = "1";
+            this.Text = "2200056-6";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
     }
 }
