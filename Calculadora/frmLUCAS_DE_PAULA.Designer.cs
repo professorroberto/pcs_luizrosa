b9df01f433bd7c24716607315f05db1368d0073b
lucas-mgrj <79200190+lucas-mgrj@users.noreply.github.com>
2021-08-19 08:32:55 -0300

    corrida 6

--- Calculadora/frmLUCAS_DE_PAULA.Designer.cs
+++ Calculadora/frmLUCAS_DE_PAULA.Designer.cs
@@ -43,32 +43,35 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
             this.button1.Location = new System.Drawing.Point(12, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "Somar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
             this.button2.Location = new System.Drawing.Point(121, 100);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(103, 25);
             this.button2.TabIndex = 1;
             this.button2.Text = "Multiplicar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.LightSteelBlue;
             this.button3.Location = new System.Drawing.Point(230, 100);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(103, 25);
             this.button3.TabIndex = 2;
             this.button3.Text = "Subtrair";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
