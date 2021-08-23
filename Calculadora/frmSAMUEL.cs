e6411c2fb9ca7ff3b521a33b46c573d14a65488f
Bruno-Sacciotti <brunosacciotti473@gmail.com>
2021-08-23 08:15:58 -0300

    Corrida 13

--- Calculadora/frmSAMUEL.cs
+++ Calculadora/frmSAMUEL.cs
@@ -32,7 +32,7 @@ namespace Calculadora
             int n2 = int.Parse(Num2.Text);
             int n3 = int.Parse(Num3.Text);
             int result = n1 - n2 - n3;
-            label4.Text = result.ToString;
+
 
         }
 
