d91bd085a142caef811f3655f5626edbaabcb65e
samuel martinelli <samuelmartinelli01@gmail.com>
2021-08-23 08:15:57 -0300

    correção no codigo

--- Calculadora/frmSAMUEL.Designer.cs
+++ Calculadora/frmSAMUEL.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Num3 = new System.Windows.Forms.TextBox();
-            this.Num2 = new System.Windows.Forms.TextBox();
+            this.Nume3 = new System.Windows.Forms.TextBox();
+            this.Nume2 = new System.Windows.Forms.TextBox();
             this.Num1 = new System.Windows.Forms.TextBox();
             this.n1 = new System.Windows.Forms.Label();
             this.n2 = new System.Windows.Forms.Label();
@@ -38,27 +38,32 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Nume1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Num3
+            // Nume3
             // 
-            this.Num3.Location = new System.Drawing.Point(288, 40);
-            this.Num3.Name = "Num3";
-            this.Num3.Size = new System.Drawing.Size(106, 20);
-            this.Num3.TabIndex = 0;
+            this.Nume3.Font = new System.Drawing.Font("Lucida Sans", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nume3.Location = new System.Drawing.Point(288, 40);
+            this.Nume3.Name = "Nume3";
+            this.Nume3.Size = new System.Drawing.Size(106, 39);
+            this.Nume3.TabIndex = 0;
             // 
-            // Num2
+            // Nume2
             // 
-            this.Num2.Location = new System.Drawing.Point(158, 40);
-            this.Num2.Name = "Num2";
-            this.Num2.Size = new System.Drawing.Size(99, 20);
-            this.Num2.TabIndex = 1;
+            this.Nume2.Font = new System.Drawing.Font("Lucida Sans", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nume2.Location = new System.Drawing.Point(158, 40);
+            this.Nume2.Name = "Nume2";
+            this.Nume2.Size = new System.Drawing.Size(99, 39);
+            this.Nume2.TabIndex = 1;
             // 
             // Num1
             // 
             this.Num1.Location = new System.Drawing.Point(27, 40);
             this.Num1.Name = "Num1";
-            this.Num1.Size = new System.Drawing.Size(100, 20);
+            this.Num1.Size = new System.Drawing.Size(0, 20);
             this.Num1.TabIndex = 2;
             // 
             // n1
@@ -121,18 +126,51 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.DarkGreen;
-            this.label4.Location = new System.Drawing.Point(181, 160);
+            this.label4.Location = new System.Drawing.Point(176, 160);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Resultado";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button4.Location = new System.Drawing.Point(133, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 44);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Maior";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Nume1
+            // 
+            this.Nume1.Font = new System.Drawing.Font("Lucida Sans", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nume1.Location = new System.Drawing.Point(13, 40);
+            this.Nume1.Name = "Nume1";
+            this.Nume1.Size = new System.Drawing.Size(114, 39);
+            this.Nume1.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button5.Location = new System.Drawing.Point(138, 248);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 37);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "MENOR";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
             // frmSAMUEL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
             this.ClientSize = new System.Drawing.Size(427, 337);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Nume1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -141,8 +179,8 @@
             this.Controls.Add(this.n2);
             this.Controls.Add(this.n1);
             this.Controls.Add(this.Num1);
-            this.Controls.Add(this.Num2);
-            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Nume2);
+            this.Controls.Add(this.Nume3);
             this.Name = "frmSAMUEL";
             this.Text = "22000728";
             this.Load += new System.EventHandler(this.frmSAMUEL_Load);
@@ -153,8 +191,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox Num3;
-        private System.Windows.Forms.TextBox Num2;
+        private System.Windows.Forms.TextBox Nume3;
+        private System.Windows.Forms.TextBox Nume2;
         private System.Windows.Forms.TextBox Num1;
         private System.Windows.Forms.Label n1;
         private System.Windows.Forms.Label n2;
@@ -163,5 +201,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox Nume1;
+        private System.Windows.Forms.Button button5;
     }
 }
