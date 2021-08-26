59d3ba899f9173f69704e1aabd858b8daeffb0ea
Matheus Ferreira <mf7216329@gmail.com>
2021-08-26 07:16:42 -0300

    teste

--- Calculadora/frmGUILHERME.Designer.cs
+++ Calculadora/frmGUILHERME.Designer.cs
@@ -49,7 +49,7 @@
             this.label1.Location = new System.Drawing.Point(13, 64);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 39);
+            this.label1.Size = new System.Drawing.Size(175, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Número 1";
             // 
@@ -60,7 +60,7 @@
             this.label2.Location = new System.Drawing.Point(399, 64);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(181, 39);
+            this.label2.Size = new System.Drawing.Size(175, 38);
             this.label2.TabIndex = 1;
             this.label2.Text = "Número 3";
             // 
@@ -71,7 +71,7 @@
             this.label3.Location = new System.Drawing.Point(202, 64);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(181, 39);
+            this.label3.Size = new System.Drawing.Size(175, 38);
             this.label3.TabIndex = 2;
             this.label3.Text = "Número 2";
             // 
@@ -170,6 +170,7 @@
             this.btnMenor.TabIndex = 11;
             this.btnMenor.Text = "MENOR";
             this.btnMenor.UseVisualStyleBackColor = false;
+            this.btnMenor.Click += new System.EventHandler(this.btnMenor_Click);
             // 
             // frmGUILHERME
             // 
