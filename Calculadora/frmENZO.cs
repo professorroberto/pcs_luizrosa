74b9758663407baf6e8f004280a5453f6868af5a
EnzoSylvestrin <crysis6699@gmail.com>
2021-08-26 08:16:31 -0300

    corrida  20

--- Calculadora/frmENZO.cs
+++ Calculadora/frmENZO.cs
@@ -19,11 +19,16 @@ namespace Calculadora
         private void button3_Click(object sender, EventArgs e)
         {
             lblresultado.Text = Convert.ToString(int.Parse(txtN1.Text) * int.Parse(txtN2.Text) * int.Parse(txtN3.Text));
+            string res = Convert.ToString(int.Parse(txtN1.Text) * int.Parse(txtN2.Text) * int.Parse(txtN3.Text));
+            msg("Multiplicar", res);
         }
 
         private void btnSomar_Click(object sender, EventArgs e)
         {
             lblresultado.Text = Convert.ToString(int.Parse(txtN1.Text) + int.Parse(txtN2.Text) + int.Parse(txtN3.Text));
+            string res = Convert.ToString(int.Parse(txtN1.Text) + int.Parse(txtN2.Text) + int.Parse(txtN3.Text));
+            msg("Somar", res);
+
         }
 
         private void frmENZO_Load(object sender, EventArgs e)
@@ -38,6 +43,8 @@ namespace Calculadora
         private void btnSubtrair_Click(object sender, EventArgs e)
         {
             lblresultado.Text = Convert.ToString(int.Parse(txtN1.Text) - int.Parse(txtN2.Text) - int.Parse(txtN3.Text));
+            string res = Convert.ToString(int.Parse(txtN1.Text) - int.Parse(txtN2.Text) - int.Parse(txtN3.Text));
+            msg("Subtrair", res);
         }
 
         private void lblresultado_Click(object sender, EventArgs e)
@@ -47,6 +54,7 @@ namespace Calculadora
 
         private void btnMaior_Click(object sender, EventArgs e)
         {
+            
             List<int> Listinha = new List<int>();
 
             Listinha.Add(int.Parse(txtN1.Text));
@@ -54,6 +62,8 @@ namespace Calculadora
             Listinha.Add(int.Parse(txtN3.Text));
 
             MessageBox.Show( "" + Listinha.Max());
+            string res = Listinha.Max().ToString();
+            msg("Maior", res);
         }
 
         private void btnMenor_Click(object sender, EventArgs e)
@@ -65,6 +75,8 @@ namespace Calculadora
             Listinha.Add(int.Parse(txtN3.Text));
 
             MessageBox.Show("" + Listinha.Min());
+            string res = Listinha.Min().ToString();
+            msg("Menor", res);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -75,7 +87,10 @@ namespace Calculadora
 
             lblresultado.Text = "";
         }
-
+        private void msg(string op, string res)
+        {
+            MessageBox.Show("Parabéns, você acaba de executar a operação" + op + "com sucesso! ficamos muito felizes por utilizar os nossos sistemas! Conte sempre conosco! E o resultado da sua operação é" + res);
+        }
 
     }
 }
