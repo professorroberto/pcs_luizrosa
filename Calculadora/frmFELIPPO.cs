baab32cde5f2c52d7d35caa6daff5c1ee33832d6
Felippo Gabriel <felippo2610@gmail.com>
2021-08-26 08:18:29 -0300

    Corrida 20

--- Calculadora/frmFELIPPO.cs
+++ Calculadora/frmFELIPPO.cs
@@ -21,7 +21,8 @@ namespace Calculadora
             double res;
             res = double.Parse(txtnum1.Text) + double.Parse(txtnum2.Text) + double.Parse(txtnum3.Text);
             lblresult.Text = res.ToString();
-            MessageBox.Show("Parabéns você acaba de executar a opreração Adição com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre conosco! E o resultado da sua operação é 8");
+            MessageBox.Show("Parabéns, você acaba de executar a operação de ADIÇÃO com sucesso! Ficamos muito felizes por utilizar nossos sistemas! Conte sempre conosco! E o resultado da sua operação é  " + lblresult.Text);
+            
             
         }
 
@@ -37,7 +38,7 @@ namespace Calculadora
             double res;
             res = double.Parse(txtnum1.Text) * double.Parse(txtnum2.Text) * double.Parse(txtnum3.Text);
             lblresult.Text = res.ToString();
-            MessageBox.Show("Parabéns você acaba de executar a opreração Multiplicação com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre conosco! E o resultado da sua operação é 8");
+            MessageBox.Show("Parabéns, você acaba de executar a operação de MULTIPLICAÇÃO com sucesso! Ficamos muito felizes por utilizar nossos sistemas! Conte sempre conosco! E o resultado da sua operação é  " + lblresult.Text);
         }
 
         private void Btncalc_Click_1(object sender, EventArgs e)
@@ -45,7 +46,7 @@ namespace Calculadora
             double res;
             res = double.Parse(txtnum1.Text) - double.Parse(txtnum2.Text) - double.Parse(txtnum3.Text);
             lblresult.Text = res.ToString();
-            MessageBox.Show("Parabéns você acaba de executar a opreração Subtrair com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre conosco! E o resultado da sua operação é 8");
+            MessageBox.Show("Parabéns, você acaba de executar a operação de SUBTRAIR com sucesso! Ficamos muito felizes por utilizar nossos sistemas! Conte sempre conosco! E o resultado da sua operação é  " + lblresult.Text);
         }
 
         private void frmFELIPPO_Load(object sender, EventArgs e)
@@ -73,6 +74,7 @@ namespace Calculadora
                 MessageBox.Show("Número 2 é o maior");
             }
             else MessageBox.Show("Numero 3 é o maior");
+            MessageBox.Show("Parabéns, você acaba de executar a operação de MAIOR com sucesso! Ficamos muito felizes por utilizar nossos sistemas! Conte sempre conosco! E o resultado da sua operação é  " + lblresult.Text);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -89,6 +91,7 @@ namespace Calculadora
                 MessageBox.Show("Número 2 é o menor");
             }
             else MessageBox.Show("Numero 3 é o menor");
+            MessageBox.Show("Parabéns, você acaba de executar a operação de MENOR com sucesso! Ficamos muito felizes por utilizar nossos sistemas! Conte sempre conosco! E o resultado da sua operação é  " + lblresult.Text);
 
             
 
