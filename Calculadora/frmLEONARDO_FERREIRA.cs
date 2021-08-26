2e5cb1f74c163d7c177b42fae57be408757ac7d5
Leonardo Ferreira <leo.aferreira04@gmail.com>
2021-08-26 08:23:28 -0300

    Corrida 20

--- Calculadora/frmLEONARDO_FERREIRA.cs
+++ Calculadora/frmLEONARDO_FERREIRA.cs
@@ -24,16 +24,19 @@ namespace Calculadora
         private void button3_Click(object sender, EventArgs e)
         {
             label5.Text = Convert.ToString(int.Parse(txtNumero1.Text) * int.Parse(txtNumero2.Text) * int.Parse(txtNumero3.Text));
+            MessageBox.Show("Parabéns, você acaba de executar a operação de Multiplicação com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre conosco! E o resultado da sua operação é:" + label5.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             label5.Text = Convert.ToString(int.Parse(txtNumero1.Text) + int.Parse(txtNumero2.Text) + int.Parse(txtNumero3.Text));
+            MessageBox.Show("Parabéns, você acaba de executar a operação de Soma com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre conosco! E o resultado da sua operação é:" + label5.Text);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             label5.Text = Convert.ToString(int.Parse(txtNumero1.Text) - int.Parse(txtNumero2.Text) - int.Parse(txtNumero3.Text));
+            MessageBox.Show("Parabéns, você acaba de executar a operação de Subtração com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre conosco! E o resultado da sua operação é:" + label5.Text);
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -42,14 +45,14 @@ namespace Calculadora
             {
                 if (int.Parse(txtNumero1.Text) > int.Parse(txtNumero3.Text))
                 {
-                    MessageBox.Show("O Numero 1 é maior");
+                    MessageBox.Show("Parabéns, você acaba de executar a operação Maior com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre consco! E o resultado da sua operação é O Numero 1 é maior");
                 }
                 else
-                    MessageBox.Show("O Numero3 é maior");
+                    MessageBox.Show("Parabéns, você acaba de executar a operação Maior com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre consco! E o resultado da sua operação é O Numero3 é maior");
             }
             else if (int.Parse(txtNumero2.Text) > int.Parse(txtNumero3.Text))
             {
-                MessageBox.Show("O Numero2 é maior");
+                MessageBox.Show("Parabéns, você acaba de executar a operação Maior com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre consco! E o resultado da sua operação é O Numero2 é maior");
             }
         }
 
@@ -59,15 +62,14 @@ namespace Calculadora
             {
                 if (int.Parse(txtNumero3.Text) > int.Parse(txtNumero1.Text))
                 {
-                    MessageBox.Show("O Numero 1 é menor");
+                    MessageBox.Show("Parabéns, você acaba de executar a operação Menor com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre consco! E o resultado da sua operação é O Numero 1 é menor");
                 }
                 else
-                    MessageBox.Show("O Numero3 é menor");
+                    MessageBox.Show("Parabéns, você acaba de executar a operação Menor com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre consco! E o resultado da sua operação é O Numero3 é menor");
             }
             else if (int.Parse(txtNumero3.Text) > int.Parse(txtNumero2.Text))
             {
-                MessageBox.Show("O Numero2 é menor");
-
+                MessageBox.Show("Parabéns, você acaba de executar a operação Menor com sucesso! Ficamos muito felizes por utilizar os nossos sistemas! Conte sempre consco! E o resultado da sua operação é O numero 2 é maior");
             }
         }
 
