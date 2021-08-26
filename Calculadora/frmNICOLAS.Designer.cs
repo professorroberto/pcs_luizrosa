659f8c95d1ddd7e7dedc3038b9734ad0b66aa5ea
nicolas <nicolas.arao@outlook.com>
2021-08-26 07:49:03 -0300

    corrida                                                  16

--- Calculadora/frmNICOLAS.Designer.cs
+++ Calculadora/frmNICOLAS.Designer.cs
@@ -109,11 +109,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label1.Location = new System.Drawing.Point(197, 262);
+            this.label1.Location = new System.Drawing.Point(130, 281);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 29);
+            this.label1.Size = new System.Drawing.Size(240, 71);
             this.label1.TabIndex = 6;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
