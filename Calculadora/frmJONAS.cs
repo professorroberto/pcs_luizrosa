50aefaba2ff9b2a0f584e04d90a3a22465ed1b03
Jonas Comparoni Junior <jonascpj@gmail.com>
2021-09-16 20:14:51 -0300

    parte 19

--- Calculadora/frmJONAS.cs
+++ Calculadora/frmJONAS.cs
@@ -15,24 +15,26 @@ namespace Calculadora
         {
             InitializeComponent();
         }
-
+            
         private void btnmenos_Click(object sender, EventArgs e)
         {
-
-
+            int N1;
+            int N2;
+            int N3;
             if (txt1.Text == "" && txt2.Text == "" && txt3.Text == "")
             {
                 MessageBox.Show("Preencha todos os campos");
             }
             else
             {
-                int N1 = int.Parse(txt1.Text);
-                int N2 = int.Parse(txt2.Text);
-                int N3 = int.Parse(txt3.Text);
-                int result = N1 - N2 - N3;
+                N1 = int.Parse(txt1.Text);
+                N2 = int.Parse(txt2.Text);
+                N3 = int.Parse(txt3.Text);
+                result = N1 - N2 - N3;
                 lblres.Text = result.ToString();
             }
 
+            MessageBox.Show("Parabéns, você executou a operação de subtração com sucesso! O resultado é " + result.ToString());
         }
 
         private void frmJONAS_Load(object sender, EventArgs e)
@@ -42,34 +44,45 @@ namespace Calculadora
 
         private void btnvezes_Click(object sender, EventArgs e)
         {
+            int N1;
+            int N2;
+            int N3;
             if (txt1.Text == "" && txt2.Text == "" && txt3.Text == "")
             {
                 MessageBox.Show("Preencha todos os campos");
             }
             else
             {
-                int N1 = int.Parse(txt1.Text);
-                int N2 = int.Parse(txt2.Text);
-                int N3 = int.Parse(txt3.Text);
-                int result = N1 * N2 * N3;
+                N1 = int.Parse(txt1.Text);
+                N2 = int.Parse(txt2.Text);
+                N3 = int.Parse(txt3.Text);
+                result = N1 * N2 * N3;
                 lblres.Text = result.ToString();
             }
-        }
 
+            MessageBox.Show("Parabéns, você executou a operação de multiplicação com sucesso! O resultado é " + result.ToString());
+        }
+            int result;
         private void btnmais_Click(object sender, EventArgs e)
         {
+            
+            int N1;
+            int N2;
+            int N3;
             if (txt1.Text == "" && txt2.Text == "" && txt3.Text == "")
             {
                 MessageBox.Show("Preencha todos os campos");
             }
             else
             {
-                int N1 = int.Parse(txt1.Text);
-                int N2 = int.Parse(txt2.Text);
-                int N3 = int.Parse(txt3.Text);
-                int result = N1 + N2 + N3;
+                N1 = int.Parse(txt1.Text);
+                N2 = int.Parse(txt2.Text);
+                N3 = int.Parse(txt3.Text);
+                result = N1 + N2 + N3;
                 lblres.Text = result.ToString();
             }
+
+            MessageBox.Show("Parabéns, você executou a operação de adição com sucesso! O resultado é "+ result.ToString());
         }
 
         private void btnmenor_Click(object sender, EventArgs e)
@@ -87,6 +100,7 @@ namespace Calculadora
                  
            }
             lblres.Text = menor.ToString();
+            MessageBox.Show("Parabéns, você executou a operação de menor com sucesso! O resultado é " + menor.ToString());
         }
 
         private void btnmaior_Click(object sender, EventArgs e)
@@ -104,6 +118,7 @@ namespace Calculadora
 
             }
             lblres.Text = maior.ToString();
+            MessageBox.Show("Parabéns, você executou a operação de maior com sucesso! O resultado é " + maior.ToString());
         }
 
         private void btnlimpar_Click(object sender, EventArgs e)
