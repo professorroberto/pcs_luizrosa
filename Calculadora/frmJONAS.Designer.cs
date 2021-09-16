ac5e04ff8a3618ebe97bc2b70314486fa5c576f9
Jonas Comparoni Junior <jonascpj@gmail.com>
2021-09-16 18:54:59 -0300

    parte 10

--- Calculadora/frmJONAS.Designer.cs
+++ Calculadora/frmJONAS.Designer.cs
@@ -150,6 +150,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(640, 438);
             this.Controls.Add(this.btnmaior);
             this.Controls.Add(this.btnmenos);
