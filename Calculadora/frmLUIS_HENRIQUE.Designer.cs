86ddaa12aab7e31b96d44143d57a864faf5335bd
unknown <luishdelmadi@gmail.com>
2021-09-16 19:56:42 -0300

    cor fonte

--- Calculadora/frmLUIS_HENRIQUE.Designer.cs
+++ Calculadora/frmLUIS_HENRIQUE.Designer.cs
@@ -74,6 +74,7 @@
             // 
             this.lblResposta.AutoSize = true;
             this.lblResposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResposta.ForeColor = System.Drawing.Color.DarkCyan;
             this.lblResposta.Location = new System.Drawing.Point(508, 227);
             this.lblResposta.Name = "lblResposta";
             this.lblResposta.Size = new System.Drawing.Size(42, 61);
