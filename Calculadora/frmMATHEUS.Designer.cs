5754374020db5c180e2dc63d6d9037c15d272926
Matheus Araujo Ferreira <matheusferreira130804@gmail.com>
2021-09-18 16:24:12 -0300

    Ex15 e correcao Ex8

--- Calculadora/frmMATHEUS.Designer.cs
+++ Calculadora/frmMATHEUS.Designer.cs
@@ -132,8 +132,9 @@
             // lblRes
             // 
             this.lblRes.AutoSize = true;
-            this.lblRes.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblRes.BackColor = System.Drawing.Color.PaleTurquoise;
             this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.ForeColor = System.Drawing.Color.DarkCyan;
             this.lblRes.Location = new System.Drawing.Point(192, 264);
             this.lblRes.Name = "lblRes";
             this.lblRes.Size = new System.Drawing.Size(265, 61);
