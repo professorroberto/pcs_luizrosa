6f1c93954aa07ec3de5a23464f467638b02eab12
Matheus Araujo Ferreira <matheusferreira130804@gmail.com>
2021-09-18 15:32:45 -0300

    Ex11

--- Calculadora/frmMATHEUS.Designer.cs
+++ Calculadora/frmMATHEUS.Designer.cs
@@ -152,6 +152,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(496, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblRes);
