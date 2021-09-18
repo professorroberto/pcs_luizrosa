ade60f340e4977bef94ca5e5561950c9a7802b4a
Matheus Araujo Ferreira <matheusferreira130804@gmail.com>
2021-09-18 15:18:57 -0300

    Ex8

--- Calculadora/frmMATHEUS.Designer.cs
+++ Calculadora/frmMATHEUS.Designer.cs
@@ -124,6 +124,7 @@
             // lblRes
             // 
             this.lblRes.AutoSize = true;
+            this.lblRes.BackColor = System.Drawing.Color.DarkCyan;
             this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRes.Location = new System.Drawing.Point(117, 187);
             this.lblRes.Name = "lblRes";
