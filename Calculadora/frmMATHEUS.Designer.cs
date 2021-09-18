3a3da0932e8b908911fd9c871ee9e5fbee9c5b01
Matheus Araujo Ferreira <matheusferreira130804@gmail.com>
2021-09-18 16:25:19 -0300

    Ex16

--- Calculadora/frmMATHEUS.Designer.cs
+++ Calculadora/frmMATHEUS.Designer.cs
@@ -150,6 +150,7 @@
             this.button1.TabIndex = 11;
             this.button1.Text = "Maior";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button4
             // 
