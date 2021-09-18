a122d1410fb9460df48d2ed517685222adb86daa
Matheus Araujo Ferreira <matheusferreira130804@gmail.com>
2021-09-18 15:29:15 -0300

    Ex9'

--- Calculadora/frmMATHEUS.Designer.cs
+++ Calculadora/frmMATHEUS.Designer.cs
@@ -38,6 +38,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.lblRes = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtN1
@@ -132,11 +133,22 @@
             this.lblRes.TabIndex = 10;
             this.lblRes.Text = "Resultado";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Location = new System.Drawing.Point(71, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 44);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Maior";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // frmMATHEUS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblRes);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -166,6 +178,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Button button1;
 
     }
 }
