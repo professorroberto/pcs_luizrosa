9e54601ccfdd03dbffe3a34bea3452133c0c4686
Matheus Araujo Ferreira <matheusferreira130804@gmail.com>
2021-09-18 14:58:36 -0300

    Ex3

--- Calculadora/frmMATHEUS.Designer.cs
+++ Calculadora/frmMATHEUS.Designer.cs
@@ -28,38 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtN3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.lblRes = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtN1
             // 
-            this.textBox1.Location = new System.Drawing.Point(45, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(118, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtN1.Location = new System.Drawing.Point(45, 68);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(118, 20);
+            this.txtN1.TabIndex = 0;
             // 
-            // textBox2
+            // txtN2
             // 
-            this.textBox2.Location = new System.Drawing.Point(181, 68);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(123, 20);
-            this.textBox2.TabIndex = 1;
+            this.txtN2.Location = new System.Drawing.Point(181, 68);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(123, 20);
+            this.txtN2.TabIndex = 1;
             // 
-            // textBox3
+            // txtN3
             // 
-            this.textBox3.Location = new System.Drawing.Point(326, 68);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(115, 20);
-            this.textBox3.TabIndex = 2;
+            this.txtN3.Location = new System.Drawing.Point(326, 68);
+            this.txtN3.Name = "txtN3";
+            this.txtN3.Size = new System.Drawing.Size(115, 20);
+            this.txtN3.TabIndex = 2;
             // 
             // label1
             // 
@@ -88,23 +88,15 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "label3";
             // 
-            // label4
+            // btnSub
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(231, 175);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(68, 112);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(76, 45);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSub.Location = new System.Drawing.Point(68, 112);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(76, 45);
+            this.btnSub.TabIndex = 7;
+            this.btnSub.Text = "Subtrair";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
             // 
             // button2
             // 
@@ -124,21 +116,30 @@
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(211, 193);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(55, 13);
+            this.lblRes.TabIndex = 10;
+            this.lblRes.Text = "Resultado";
+            // 
             // frmMATHEUS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 450);
+            this.Controls.Add(this.lblRes);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSub);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtN3);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
             this.Name = "frmMATHEUS";
             this.Text = "frmMATHEUS Ra:2200022-5";
             this.ResumeLayout(false);
@@ -148,16 +149,16 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtN3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSub;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblRes;
 
     }
 }
